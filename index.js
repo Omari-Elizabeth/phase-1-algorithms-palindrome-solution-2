f5066f9663b797ea5b4ec9dd9b1895311996a5d2
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-08-29 13:34:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    // compare the letter we're iterating over to the corresponding letter at the end of the string
+    const j = word.length - 1 - i;
+    if (word[i] !== word[j]) {
+      // if the letters don't match, return false
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
